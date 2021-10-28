3e054f2e837e5012645d30f77970d7a5a913926b
phhuong256 <1851061398@e.tlu.edu.vn>
2021-10-28 17:07:43 +0700

    do thi co huong

---
+++ TTUD/dothicohuong.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+const int MAX = 100;
+int v, e;
+vector<int> ke[MAX];
+
+int main() {
+	cin >> v >> e;
+	
+	for(int i = 0; i < e; i++) {
+		int a, b;
+		cin >> a >> b;
+		ke[a].push_back(b);
+	}
+	
+	for(int i = 1; i <=v; i++) sort(ke[i].begin(), ke[i].end());
+	
+	for(int i =1; i <= v; i++) {
+		cout << i << ": ";
+		for(int j = 0; j < ke[i].size(); j++) cout << " " << ke[i][j];
+		cout<< endl;
+	}
+}
